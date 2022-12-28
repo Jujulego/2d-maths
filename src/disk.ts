1ccb468c0049037bd0353e042d5abd996abe1832
Julien Capellari <julien.capellari@gmail.com>
2022-12-29 00:53:52 +0100

    feat: add isDisk util

--- src/disk.ts
+++ src/disk.ts
@@ -1,19 +1,45 @@
 import { type IPoint, Point } from './point';
 import { Shape } from './shape';
 import { Rect } from './rect';
+import { isObject } from './utils';
 
 // Types
 export interface IDisk {
+  /**
+   * Center x coordinate
+   */
   cx: number;
+
+  /**
+   * Center y coordinate
+   */
   cy: number;
+
+  /**
+   * Radius
+   */
   r: number;
 }
 
 // Class
+/**
+ * Represents a disk shape, from a center and a radius
+ */
 export class Disk extends Shape implements IDisk {
   // Attributes
+  /**
+   * Center x coordinate
+   */
   cx: number;
+
+  /**
+   * Center y coordinate
+   */
   cy: number;
+
+  /**
+   * Radius
+   */
   r: number;
 
   // Constructor
@@ -36,10 +62,16 @@ export class Disk extends Shape implements IDisk {
   }
 
   // Properties
+  /**
+   * Center point
+   */
   get center(): Point {
     return new Point({ x: this.cx, y: this.cy });
   }
 
+  /**
+   * Bounding box fitted to the disk
+   */
   get bbox(): Rect {
     return new Rect({
       t: this.cy + this.r + 1,
@@ -51,8 +83,37 @@ export class Disk extends Shape implements IDisk {
 }
 
 // Utils
+/**
+ * Returns true if object is a disk object
+ * @see IDisk
+ *
+ * @param obj
+ */
+export function isDisk(obj: unknown): obj is IDisk {
+  return isObject(obj) && 'cx' in obj;
+}
+
+/**
+ * Disk easy builder (from a disk object)
+ * @see Disk
+ *
+ * @param d
+ */
 export function disk(d: IDisk): Disk;
+
+/**
+ * Disk easy builder (from a point object and a radius)
+ * @see Disk
+ *
+ * @param c Center point
+ * @param r Radius
+ */
 export function disk(c: IPoint, r: number): Disk;
+
+/**
+ * Disk easy builder
+ * @see Disk
+ */
 export function disk(...args: [IDisk] | [IPoint, number]): Disk {
   if (args.length === 1) {
     return new Disk(args[0]);
